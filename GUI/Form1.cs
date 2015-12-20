1cbbe957afd99e60ec77507fd6acc909512ed698
nanhopper <nanhopper@gmail.com>
2015-12-20 15:14:19 +0100

    add solution

---
+++ GUI/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        private Presenter Presenter = new Presenter();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.DataBindings.Clear();
+            Presenter.RetrieveAndFillGrid();
+        }
+
+        public void SetDataSourceOnList(BindingList<int> source)
+        {
+            listBox1.DataSource = source;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Presenter.SendMessage += Presenter_SendMessage;
+        }
+
+        private void Presenter_SendMessage(object sender, SendMessageArgs args)
+        {
+            Invoke(new Action(()=> label1.Text = args.Message));
+        }
+    }
+
+    public class Presenter
+    {
+        public delegate void SendMessageHandler(object sender, SendMessageArgs args);
+
+        public event SendMessageHandler SendMessage;
+
+        private void OnMessageSend(SendMessageArgs args)
+        {
+            if(SendMessage != null)
+            {
+                SendMessage(this, args);
+            }
+        }
+
+        internal void RetrieveAndFillGrid()
+        {
+            DoLongTask1();
+        }
+
+        private void DoLongTask0()
+        {
+            var results = DoWork();
+            WriteResult(results);
+        }
+
+        private async Task DoLongTask1()
+        {
+            var results = await Task.Run(() => DoWork());
+            WriteResult(results);
+        }
+
+        private async Task DoLongTask2()
+        {
+            var task = new Task<IEnumerable<int>>(() => DoWork());
+            task.Start();
+            var result = await task;
+            WriteResult(result);
+        }
+
+        private async Task DoLongTask3()
+        {
+            var results = await Task.Factory.StartNew(() => DoWork());
+            WriteResult(results);
+        }
+
+        private IEnumerable<int> DoWork()
+        {
+            WriteLine("Starting long task...");
+            foreach (var result in GetResults())
+            {
+                yield return result;
+            }
+            WriteLine("Task done.");
+        }
+
+        private void WriteResult(int result)
+        {
+            WriteLine("Long task done. Result is: " + result);
+        }
+
+        private void WriteResult(IEnumerable<int> results)
+        {
+            var i = 1;
+            foreach (var result in results)
+            {
+                WriteLine(string.Format("Intermediary result {0} is: " + result, i));
+                i++;
+            }
+        }
+
+        private void WriteLine(string message)
+        {
+            OnMessageSend(new SendMessageArgs { Message = string.Format("{0} [{1}]", message, Thread.CurrentThread.ManagedThreadId) });
+        }
+
+        private IEnumerable<int> GetResults()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                yield return random.Next();
+            }
+        }
+
+        private int GetResult()
+        {
+            return random.Next();
+        }
+
+        private Random random = new Random(DateTime.Now.Millisecond);
+    }
+
+    public class SendMessageArgs
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ Sandbox/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    class Program
+    {
+        private delegate void NotifyPropertyChangedEventHandler(object sender, NotifyEventArgs args);
+
+        private static event NotifyPropertyChangedEventHandler NotifyPropertyChanged;
+
+        static void Main(string[] args)
+        {            
+            NotifyPropertyChanged += OnNotifyPropertyChanged;
+
+            while (true)
+            {
+                var read = Console.ReadKey(true);
+                if(read.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine("Enter key pressed, exiting loop. Press Enter key again to stop program.");
+                    break;
+                }
+                WriteLine("Key pressed: " + read.Key);
+                DoLongTask1();
+            }
+            Console.ReadLine();
+        }
+
+        private static void OnNotifyPropertyChanged(object sender, NotifyEventArgs args)
+        {
+            WriteLine("Result received: " + args.Result);
+        }
+
+        private static void DoLongTask0()
+        {
+            var results = DoWork();
+            WriteResult(results);
+        }
+
+        private async static Task DoLongTask1()
+        {
+            var results = await Task.Run(() => DoWork().ToList());
+            WriteResult(results);
+        }        
+
+        private async static Task DoLongTask2()
+        {
+            var task = new Task<IEnumerable<int>>(() => DoWork());
+            task.Start();
+            var result = await task;
+            WriteResult(result);
+        }
+
+        private async static Task DoLongTask3()
+        {
+            var results = await Task.Factory.StartNew(()=> DoWork());
+            WriteResult(results);
+        }
+
+        private static IEnumerable<int> DoWork()
+        {
+            WriteLine("Starting long task...");
+            foreach(var result in GetResults())
+            {
+                yield return result;
+            }
+            WriteLine("Task done.");
+        }
+
+        private static void WriteResult(int result)
+        {
+            WriteLine("Long task done. Result is: " + result);
+        }
+
+        private static void WriteResult(IEnumerable<int> results)
+        {
+            foreach (var result in results)
+            {
+                NotifyPropertyChanged(null, new NotifyEventArgs { Result = result.ToString() });
+                //WriteLine("Intermediary result is: " + result);
+            }
+        }
+
+        private static void WriteLine(string message)
+        {
+            Console.WriteLine(string.Format("{0} [{1}]", message, Thread.CurrentThread.ManagedThreadId));
+        }
+
+        private static IEnumerable<int> GetResults()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Thread.Sleep(3000);
+                yield return random.Next();
+            }
+        }
+
+        private static int GetResult()
+        {
+            return random.Next();
+        }
+
+        private static Random random = new Random(DateTime.Now.Millisecond);
+
+        private class NotifyEventArgs
+        {
+            public string Result { get; set; }
+        }
+    }
+}
